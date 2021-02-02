5654051ca99d524fe95048a6a645c20eeed85cbb
zschorlichjohan <zschorlichjohann@gmail.com>
2021-02-02 21:03:01 +0000

    Bot Init and First Command

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import discord
+import os
+
+client = discord.Client()
+
+@client.event
+
+async def on_ready():
+  print('Eingeloggt als {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+
+  if message.content.startswith("§help"):
+    await message.channel.send("SoonTM")
+
+client.run(os.getenv('TOKEN'))
